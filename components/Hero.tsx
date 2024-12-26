429c5844fc3812cf8dda03af0f667733e015483f
Cristina Lester <cristina.deelester@gmail.com>
2024-12-26 08:28:39 -0800

    added grid bg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,30 +3,32 @@ import { FaLocationArrow } from 'react-icons/fa6';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 import { MagicButton } from './ui/MagicButton';
 import { BackgroundBeamsWithCollision } from './ui/ExplodingBeams';
+import { DotBackground } from './ui/DotBackground';
 
 export const Hero = () => {
   return (
-    <BackgroundBeamsWithCollision>
-      <div className='flex justify-center relative z-10'>
-        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-          <h1>
-            <TextGenerateEffect
-              words='Hi! I am Cristina Lester.'
-              className='text-center text-[40px] md:text-5xl lg:text-6xl'
-            />
-          </h1>
-          <h2 className='my-5'>
-            <TextGenerateEffect
-              words=' I craft fabulous user experiences ✨'
-              className=' uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'
-              duration={3}
-            />
-          </h2>
-          <a href='#about' className='block mt-5'>
+    <>
+      <BackgroundBeamsWithCollision>
+        <div className='flex flex-col items-center justify-center relative z-10'>
+          <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+            <h1>
+              <TextGenerateEffect
+                words='Hi! I am Cristina Lester.'
+                className='text-center text-[40px] md:text-5xl lg:text-6xl'
+              />
+            </h1>
+            <h2 className='mt-5 mb-20'>
+              <TextGenerateEffect
+                words=' I craft fabulous user experiences ✨'
+                className=' uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'
+                duration={3}
+              />
+            </h2>
             <MagicButton title='Check out my work' icon={<FaLocationArrow />} />
-          </a>
+          </div>
         </div>
-      </div>
-    </BackgroundBeamsWithCollision>
+      </BackgroundBeamsWithCollision>
+      <DotBackground />
+    </>
   );
 };
